8475b80e3ac0c1ff042677f244c656ac43299f5d
Tkadha(home) <emforhs228@naver.com>
2023-09-25 19:40:51 +0900

    image upload

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,9 +3,9 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+character = load_image('sprite_sheet.png')
 def handle_events():
-    global running, LR, UD
+    global running, LR, UD, move
 
     events = get_events()
     for event in events:
@@ -13,7 +13,7 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
-                running = False;
+                running = False
             elif event.key == SDLK_RIGHT:
                 LR += 1
             elif event.key == SDLK_LEFT:
@@ -36,20 +36,21 @@ def handle_events():
 running = True
 frame = 0
 LR, UD = 0, 0
+move = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 
 while running:
     clear_canvas()
     TUK.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    character.clip_draw(frame * 65, 0, 60, 60, x, y)
+    frame = (frame + 1) % 10
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 8
     if 10 < x + LR * 10 < TUK_WIDTH-10:
         x += LR * 10
     if 25 < y + UD * 10 < TUK_HEIGHT-25:
         y += UD * 10
-    delay(0.05)
+    delay(0.07)
 
 
 close_canvas()
