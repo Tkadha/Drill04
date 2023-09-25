cc353ac1c68b6d6b3196d2336ca6f5e01d006e15
Tkadha(home) <emforhs228@naver.com>
2023-09-25 18:49:15 +0900

    make move

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,6 +5,34 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK = load_image('TUK_GROUND.png')
 
 
+def handle_events():
+    global running, LR, UD
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False;
+            elif event.key == SDLK_RIGHT:
+                LR += 1
+            elif event.key == SDLK_LEFT:
+                LR -= 1
+            elif event.key == SDLK_UP:
+                UD += 1
+            elif event.key == SDLK_DOWN:
+                UD -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                LR -= 1
+            elif event.key == SDLK_LEFT:
+                LR += 1
+            elif event.key == SDLK_UP:
+                UD -= 1
+            elif event.key == SDLK_DOWN:
+                UD += 1
+
 
 running = True
 x = 800 // 2
