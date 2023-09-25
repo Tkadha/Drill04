02216bb5defcb3598457fe8cc58b06875a2cac49
Tkadha(home) <emforhs228@naver.com>
2023-09-25 18:48:44 +0900

    upload file

---
+++ move_my_character_with_key.py
@@ -0,0 +1,16 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+TUK = load_image('TUK_GROUND.png')
+
+
+
+running = True
+x = 800 // 2
+frame = 0
+
+# fill here
+
+
+close_canvas()
